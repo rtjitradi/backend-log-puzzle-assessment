e829d690bfcc7c11aa1b8e45a668a6ca701ffa59
Reginald Tjitradi <rtjitradi@gmail.com>
2020-07-17 10:57:43 -0700

    wooo hoo that's my bunny

--- logpuzzle.py
+++ logpuzzle.py
@@ -14,11 +14,15 @@ HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US;
 rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
 """
 
+__author__ = "Reggy Tjitradi guided by Stew with support from Ramon and Ruth"
+
+
 import os
 import re
 import sys
 import urllib.request
 import argparse
+from collections import defaultdict  # https://docs.python.org/3/library/collections.html#collections.defaultdict
 
 
 def read_urls(filename):
@@ -26,8 +30,32 @@ def read_urls(filename):
     extracting the hostname from the filename itself, sorting
     alphabetically in increasing order, and screening out duplicates.
     """
-    # +++your code here+++
-    pass
+
+    #  https://www.youtube.com/watch?v=Nn2KQmVF5Og&feature=emb_logo
+    #  We need a dictionary lists to use the key and value, so import collections and use defaultdict
+    list_of_puzzle = []
+    puzzle = defaultdict(list)
+    pattern = r"\S*\Spuzzle\S*"
+    temp = filename.split("_")
+    address = "HTTP://" + temp[1]
+    with open(filename)as f:
+        for line in f:
+            url_found = re.search(pattern, line)
+            if url_found:
+                puzzle[line[url_found.start():url_found.end()]]  # https://docs.python.org/3/library/re.html#re.match.start
+    for key in puzzle.keys():
+        list_of_puzzle.append(address + key)
+    if temp[0] == "animal":
+        return sorted(list_of_puzzle)
+    else:
+        return sorted(list_of_puzzle, key=lambda x: x.split("_")[4])
+
+
+def html_tag(tag):
+    def wrap_text(file, msg):
+        with open(file, "a") as f:
+            f.write('<{0} src="{1}" />'.format(tag, msg))
+    return wrap_text
 
 
 def download_images(img_urls, dest_dir):
@@ -38,8 +66,20 @@ def download_images(img_urls, dest_dir):
     to show each local image file.
     Creates the directory if necessary.
     """
-    # +++your code here+++
-    pass
+
+    # https://docs.python.org/3/library/os.html
+    img_count = 0
+    try:
+        os.mkdir(dest_dir)
+    except OSError as err:
+        print(err)
+        exit(1)
+    img_tag = html_tag("img")
+    for url in img_urls:
+        img_filename = os.path.join(dest_dir, "img" + str(img_count) + ".jpg")
+        urllib.request.urlretrieve(url, img_filename)
+        img_tag("index.html", img_filename)
+        img_count += 1
 
 
 def create_parser():
